10144f4c675fae298afeaaf2ab3fc56c71d5c120
Muhammad Rifqil Khanif <115907124+Rifqil-Khanif@users.noreply.github.com>
2023-05-23 20:07:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rest Api</title>
+    <style>
+        body {
+            background-color: #f2f2f2;
+            font-family: Arial, sans-serif;
+        }
+        
+        h1 {
+            color: #333;
+        }
+        
+        center {
+            margin-top: 50px;
+        }
+        
+        button {
+            padding: 10px 20px;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            cursor: pointer;
+            margin-bottom: 10px;
+        }
+        
+        button:hover {
+            background-color: #0056b3;
+        }
+    </style>
 </head>
 <body>
-<center>
+    <center>
         <h1>Rest Api</h1>
         <a href="kirimdata.php"><button>KIRIM DATA</button></a>
         <a href="tampildata.php"><button>TAMPIL DATA</button></a>
